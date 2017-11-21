c0536c054f78a7214eaa7449d4ce77ebe7015932
Stevan Little <stevan.little@gmail.com>
2017-11-21 17:10:52 +0100

    make it possible to pass in a running interpreter to the inspector and then see what happens, this can be useful when trying to see how multiple components interact, etc.

--- lib/HTML/MasonX/Inspector.pm
+++ lib/HTML/MasonX/Inspector.pm
@@ -20,46 +20,61 @@ our @ISA; BEGIN { @ISA = ('UNIVERSAL::Object') }
 our %HAS; BEGIN {
     %HAS = (
         # ... private
-        _mason_args  => sub {},
+        _mason_args  => sub { +[] },
         _interpreter => sub {},
     )
 }
 
-# all args are mason args
-sub BUILDARGS { +{ _mason_args => [ @_[ 1 .. $#_ ] ] } }
+sub BUILDARGS {
+    my $class = shift;
+    if ( scalar @_ == 1 ) {
+        # attach to a running interpreter ...
+        Carp::confess('You can either pass an instance of `HTML::Mason::Interp` or a list of key-value args for its constructor')
+            unless Scalar::Util::blessed( $_[0] )
+                && $_[0]->isa('HTML::Mason::Interp');
+
+        return +{ _interpreter => $_[0] };
+    }
+    else {
+        # all args are mason args
+        return +{ _mason_args => [ @_[ 1 .. $#_ ] ] };
+    }
+}
 
 sub BUILD {
     my ($self, $params) = @_;
 
-    # make the interpreter, but first
-    # prepare the $params to pass to
-    # Mason, and since we will alter it
-    # we make a copy ...
-    my %mason_args = @{ $self->{_mason_args} };
+    if ( not defined $self->{_interpreter} ) {
+        # make the interpreter, but first
+        # prepare the $params to pass to
+        # Mason, and since we will alter it
+        # we make a copy ...
+        my %mason_args = @{ $self->{_mason_args} };
 
-    # at least make sure they set up comp_root
-    Carp::confess( 'Cannot create a new Mason Interpreter unless you supply `comp_root` parameter' )
-        unless $mason_args{comp_root};
+        # at least make sure they set up comp_root
+        Carp::confess( 'Cannot create a new Mason Interpreter unless you supply `comp_root` parameter' )
+            unless $mason_args{comp_root};
 
-    Carp::confess( 'The `comp_root` must be a valid directory' )
-        unless -e $mason_args{comp_root} && -d $mason_args{comp_root};
+        Carp::confess( 'The `comp_root` must be a valid directory' )
+            unless -e $mason_args{comp_root} && -d $mason_args{comp_root};
 
-    # prep the comp_root before passing to Mason ...
-    $mason_args{comp_root} = $mason_args{comp_root}->stringify
-        if Scalar::Util::blessed( $mason_args{comp_root} )
-        && $mason_args{comp_root}->isa('Path::Tiny');
+        # prep the comp_root before passing to Mason ...
+        $mason_args{comp_root} = $mason_args{comp_root}->stringify
+            if Scalar::Util::blessed( $mason_args{comp_root} )
+            && $mason_args{comp_root}->isa('Path::Tiny');
 
-    # ... build an interpreter ...
+        # ... build an interpreter ...
 
-    my $interpreter = HTML::Mason::Interp->new( %mason_args )
-        || die "Could not load Mason Interpreter";
+        my $interpreter = HTML::Mason::Interp->new( %mason_args )
+            || die "Could not load Mason Interpreter";
 
-    # then set up the minimum needs to mock this run ...
-    $interpreter->set_global(
-        $_ => HTML::MasonX::Inspector::__EVIL__->new
-    ) foreach map s/^[$@%]//r, $interpreter->compiler->allow_globals; #/
+        # then set up the minimum needs to mock this run ...
+        $interpreter->set_global(
+            $_ => HTML::MasonX::Inspector::__EVIL__->new
+        ) foreach map s/^[$@%]//r, $interpreter->compiler->allow_globals; #/
 
-    $self->{_interpreter} = $interpreter;
+        $self->{_interpreter} = $interpreter;
+    }
 }
 
 ## accessor ...
