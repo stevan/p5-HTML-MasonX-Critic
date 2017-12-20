c53f3971a6c8e3ebc342c43323b1e0799905b479
Stevan Little <stevan.little@booking.com>
2017-12-20 19:50:37 +0100

    Re-use some older git-blame parsing code I had lying around.

--- lib/HTML/MasonX/Critic/Violation/BlameFile.pm
+++ lib/HTML/MasonX/Critic/Violation/BlameFile.pm
@@ -68,8 +68,37 @@ sub get_all_lines {
 sub _convert_blame_lines {
     my ($self, $blame_line) = @_;
 
-    my ($sha, $metadata, $line_num, $line) = ($blame_line =~ /^([a-f0-9]+) \((.*) (\d+)\) (.*)$/);
-    my ($author, $date) = ($metadata =~ /^(.*) (\d\d\d\d\-\d\d\-\d\d .*)$/);
+
+    my ($sha, $info, $line) = ($blame_line =~ /([0-9a-f]+)\s[A-Za-z0-9_\.\/]*\s*\((\w*[^\)]*)\)\s(.*)$/);
+
+    # first we ...
+    # we match the line number
+    my ($line_num) = ($info =~ /\s+(\d+)$/);
+    # then strip off the line number
+    $info =~ s/\s+\d+$//;
+    # next we ...
+    # extract the date by anchoring
+    # from the rear of the string
+    my ($date) = ($info =~ /([0-9-:+\s]*)$/);
+
+    # find out how much padding is
+    # in front of the date, we need
+    # to make it to the author name
+    my ($padding) = ($date =~ /^(\s*)/);
+    # then we ...
+    # make sure to trim the leading
+    # spaces that come along
+    $date =~ s/^\s*//;
+
+    # then we ...
+    # use that same regexp to remove
+    # the date so we are left with
+    # only the author's name
+    $info =~ s/([0-9-:+\s]*)$//;
+
+    utf8::decode($info);
+
+    my $author = $info.$padding;
 
     return HTML::MasonX::Critic::Violation::BlameFile::Line->new(
         line     => "$line\n", # put the \n back on ...
